b8bfc60daa59dbefc5d962bf881b9d40f4171cbc
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2025-07-04 14:54:46 +0200

    Implement AppDbContext with DbSet properties for Event, Participant, and Location

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,1 +1,12 @@
-public class AppDbContext { }
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using precio_summer_project.Models;
+
+namespace precio_summer_project.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User>(options)
+{
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Participant> Participants { get; set; }
+    public DbSet<Location> Locations { get; set; }
+}
