6f0138e02bbbac633c36748aab203fb642f11a4f
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2025-07-04 14:55:01 +0200

    Add DbContext configuration for AppDbContext with PostgreSQL connection

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using precio_summer_project.Data;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseString"))
+);
 builder.Services.AddControllers();
 
 var app = builder.Build();
