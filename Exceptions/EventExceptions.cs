c7456b9c5effc2582a81db3dd57b9afbff2fc454
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2025-07-04 18:59:35 +0200

    Add custom exception classes for event validation

---
+++ Exceptions/EventExceptions.cs
@@ -0,0 +1,26 @@
+namespace precio_summer_project.Exceptions;
+
+public class EventNameEmptyException(string exception = "Event name can't be empty")
+    : Exception(exception);
+
+public class EventDescriptionEmptyException(string exception = "Event description can't be empty")
+    : Exception(exception);
+
+public class EventLocationEmptyException(string exception = "Event location can't be empty")
+    : Exception(exception);
+
+public class EventStartDateException(
+    string exception = "Event starting date can't be earlier than todays date"
+) : Exception(exception);
+
+public class EventStartTimeException(
+    string exception = "Event starting time can't be earlier than todays time"
+) : Exception(exception);
+
+public class EventEndTimeException(
+    string exception = "Event ending time can't be earlier than event starting time"
+) : Exception(exception);
+
+public class EventEndDateException(
+    string exception = "Event ending date can't be earlier than event starting date"
+) : Exception(exception);
