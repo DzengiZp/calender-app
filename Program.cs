dc286c655928ea173f66f4532e71d1246275eb03
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2025-07-04 18:58:35 +0200

    Refactor Program.cs to streamline service registrations and middleware setup

--- Program.cs
+++ Program.cs
@@ -1,14 +1,29 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using precio_summer_project.Data;
+using precio_summer_project.Models;
+using precio_summer_project.Repositories.Events;
+using precio_summer_project.Services.Events;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseString"));
+});
+
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+
+builder.Services.AddAuthorization();
+builder
+    .Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseString"))
-);
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -19,8 +34,14 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
-app.MapControllers();
+app.UseCors(x =>
+    x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).AllowCredentials()
+);
 
-app.UseHttpsRedirection();
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
