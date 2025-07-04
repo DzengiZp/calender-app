7f5dfbf11dde22edc0b65f01616dc81a3bf3be6e
Dzengiz Prentic <dzengiz.prentic@hotmail.com>
2025-07-04 14:55:34 +0200

    Add initial migration and update database schema with Event and Location models
    - Created initial migration for the database schema including AspNetRoles, AspNetUsers, Events, Participants, and their relationships.
    - Added Event model with properties for event details including start and end dates, location, and user association.
    - Introduced Location model to store user-specific locations.
    - Updated the AppDbContextModelSnapshot to reflect the new schema.

---
+++ Migrations/20250704114537_AddedLocationModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace precio_summer_project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLocationModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EventDate",
+                table: "Events",
+                newName: "EventDateStart");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EventDateEnd",
+                table: "Events",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInPerson",
+                table: "Events",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Locations_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_UserId",
+                table: "Locations",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "EventDateEnd",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "IsInPerson",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "EventDateStart",
+                table: "Events",
+                newName: "EventDate");
+        }
+    }
+}
